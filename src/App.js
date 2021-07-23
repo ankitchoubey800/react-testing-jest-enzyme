0f94ebef50177f413541932e72dd397e03beb2bf
ankitchoubey800 <ankitchoubey800@gmail.com>
2021-07-23 21:22:40 +0530

    Writing First Tests

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/header";
+import './app.scss';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.scss';
+
+const Header=(props)=>{
+    return (
+        <header>
+            <div className="wrap">
+            <div className="logo">
+            <div className="img">
+            <h2>Logo</h2>
+            </div>                
+            </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
