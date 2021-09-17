f27654909a219e6496062c3f8e96a4b49a7d276b
ankit choubey <ankitchoubey800@gmail.com>
2021-09-17 17:19:56 +0530

    BeforeEach and Refactoring the code

--- src/components/header/header.spec.js
+++ src/components/header/header.spec.js
@@ -2,14 +2,24 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import Header from './index';
 
-it('It should render without errors',()=>{
-expect(1).toBe(1);
-});
+const setUp=(props={})=>{
+    //this is called shallow rendering where you get the exact component
+    const component=shallow(<Header {...props}/>); 
+    return component;
+};
 
 describe("Header Component",()=>{
+    let component;
+    //beforeEach function runs before running all test functions
+    beforeEach(()=>{
+        component=setUp();
+    });
+
+    it('A simple test example',()=>{
+        expect(1).toBe(1);
+        });
+
     it('This method checks if class .headerComponent is used only in one element in <Header/>',()=>{
-        //this is called shallow rendering where you get the exact component
-        const component=shallow(<Header/>); 
         //console.log(component.debug());
         const wrapper=component.find('.headerComponent');
         expect(wrapper.length).toBe(1);
